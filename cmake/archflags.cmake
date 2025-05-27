3e318ce8a34f441a37d115ea4739f79a9888d985
Albert Andaluz <albertandaluz@hotmail.com>
2025-05-27 12:43:13 +0200

    use CMAKE__SYSTEM_PROCESSOR instead of CMAKE_HOST_SYSTEM_PROCESSOR for hypothetical cross compile support
    use caret diagnostics with msvc
    added a combo option in cmakegui to manually select arch flags on windows (default:avx2 & armv8.2) an linux (default:native)

--- cmake/archflags.cmake
+++ cmake/archflags.cmake
@@ -1,15 +1,22 @@
 function(add_optimization_flags TARGET_NAME)    
+    # Add architecture-specific optimization flags for the target
     if (MSVC)        
-        if (${CMAKE_HOST_SYSTEM_PROCESSOR} STREQUAL "AMD64")
-            target_compile_options(${TARGET_NAME} PRIVATE $<$<COMPILE_LANGUAGE:CXX>:/arch:AVX2>)
-        endif()
-        if (${CMAKE_HOST_SYSTEM_PROCESSOR} STREQUAL "ARM64")
+    
+        if (${CMAKE_SYSTEM_PROCESSOR} STREQUAL "AMD64")
+            SET(ARCH_FLAGS "AVX2" CACHE STRING "instrucion set to use")
+            SET_PROPERTY(CACHE ARCH_FLAGS PROPERTY STRINGS AVX2 AVX512 AVX10.1) 
+            option(ARCH_FLAGS "CPU arch" "AVX2")
+            target_compile_options(${TARGET_NAME} PRIVATE $<$<COMPILE_LANGUAGE:CXX>:/arch:${ARCH_FLAGS}>)        
+        elseif(${CMAKE_SYSTEM_PROCESSOR} STREQUAL "ARM64")    
             target_compile_options(${TARGET_NAME} PRIVATE $<$<COMPILE_LANGUAGE:CXX>:/arch:armv8.2>)         
         endif()
     endif()
     if (UNIX)             
-        #build for native host architecture  for maximum performance (might not work on all systems)   
-        #works both for x86_64 and aarch64                
-            target_compile_options(${TARGET_NAME} PRIVATE $<$<COMPILE_LANGUAGE:CXX>:-march=native>)
+        #default> build for native host architecture  for maximum performance (might not work on all systems)   
+        #works both for x86_64 and aarch64      
+        #we currently don't test avx10.1 with gcc11 on ubuntu 22.04
+        SET_PROPERTY(CACHE ARCH_FLAGS PROPERTY STRINGS native haswell skylake-avx512) 
+        option(ARCH_FLAGS "CPU arch" "native")          
+            target_compile_options(${TARGET_NAME} PRIVATE $<$<COMPILE_LANGUAGE:CXX>:-march=${ARCH_FLAGS}>)
     endif()
 endfunction()
