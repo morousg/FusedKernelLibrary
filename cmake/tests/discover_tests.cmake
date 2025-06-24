fcc9b3e55ba671f3759a5e5e90b55050c4eb79fa
Albert Andaluz <albertandaluz@hotmail.com>
2025-06-24 20:01:29 +0200

    instead of generating headers, share code via tests_saturate.h and instantiate each input type

--- cmake/tests/discover_tests.cmake
+++ cmake/tests/discover_tests.cmake
@@ -11,42 +11,28 @@ function (discover_tests DIR)
     foreach(TEST_SOURCE ${TEST_SOURCES})         
         get_filename_component(TARGET_NAME ${TEST_SOURCE} NAME_WE)           
         file (READ ${TEST_SOURCE} TEST_SOURCE_CONTENTS ) #read the contents of the test source file
-       
+        string(FIND "${TEST_SOURCE_CONTENTS}" "AUX_HEADER"  AUX_HEADER_POS)
+        if (${AUX_HEADER_POS} GREATER_EQUAL  0)
+            continue()
+        endif()
         string(FIND "${TEST_SOURCE_CONTENTS}" "ONLY_CU"  POS_ONLY_CU)
         string(FIND "${TEST_SOURCE_CONTENTS}" "ONLY_CPU"  POS_ONLY_CPU)        
         cmake_path(GET TEST_SOURCE RELATIVE_PART DIR_RELATIVE_PATH)     
         string(REPLACE "${PROJECT_NAME}/" " " DIR_RELATIVE_PATH "${DIR_RELATIVE_PATH}") #remove the project name from the relative path
-         
- 
-
-        string(FIND "${TEST_SOURCE}" ".in"  IS_CMAKE_GENERATED_SOURCE)
-        if (${IS_CMAKE_GENERATED_SOURCE} GREATER -1)            
-            set (FUNDAMENTAL_TYPES uchar char ushort short uint int ulong long ulonglong longlong float double) 
-            foreach(FUNDAMENTAL_TYPE ${FUNDAMENTAL_TYPES})                
-                set(TEST_GENERATED_SOURCE_N "${CMAKE_CURRENT_BINARY_DIR}/${TARGET_NAME}/${TARGET_NAME}_${FUNDAMENTAL_TYPE}.h") #use the same name as the target	)       
-                configure_file(${TEST_SOURCE} ${TEST_GENERATED_SOURCE_N} @ONLY) #replace variables in the test source file                                                                         
-             #   message(STATUS   "Adding test: ${TARGET_NAME}_${FUNDAMENTAL_TYPE} from ${TEST_GENERATED_SOURCE_N} and ${TEST_SOURCE}")
-               add_generated_test("${TARGET_NAME}_${FUNDAMENTAL_TYPE}" "${TEST_GENERATED_SOURCE_N}" "cpp" "${DIR_RELATIVE_PATH}")   
-               if (CMAKE_CUDA_COMPILER AND ENABLE_CUDA)               
-                   add_generated_test("${TARGET_NAME}_${FUNDAMENTAL_TYPE}"  "${TEST_GENERATED_SOURCE_N}" "cu"  "${DIR_RELATIVE_PATH}")
-                   add_cuda_to_test("${TARGET_NAME}_${FUNDAMENTAL_TYPE}_cu")                       
-               endif()
-            endforeach()
-        else()
-            if (${POS_ONLY_CU} EQUAL -1) #if the source file does not contain "__ONLY_CU__"    
+          
+        if (${POS_ONLY_CU} EQUAL -1) #if the source file does not contain "__ONLY_CU__"    
                 if (${ENABLE_CPU})                    
                     add_generated_test("${TARGET_NAME}" "${TEST_SOURCE}" "cpp" "${DIR_RELATIVE_PATH}")                
                 endif()
             endif()
 
-            if (CMAKE_CUDA_COMPILER AND ENABLE_CUDA)
-                if (${POS_ONLY_CPU} EQUAL -1) #if the source file does not contain "__ONLY_CPU__"
+        if (CMAKE_CUDA_COMPILER AND ENABLE_CUDA)
+            if (${POS_ONLY_CPU} EQUAL -1) #if the source file does not contain "__ONLY_CPU__"
                     add_generated_test("${TARGET_NAME}"  "${TEST_SOURCE}" "cu"  "${DIR_RELATIVE_PATH}")
                     add_cuda_to_test("${TARGET_NAME}_cu")                           
-                endif()
             endif()
-        
         endif()
+ 
         
          
       
